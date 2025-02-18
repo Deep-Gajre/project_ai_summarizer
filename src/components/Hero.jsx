23f541940f9bc6b7cf66bd484185cc174c5d1285
Deep-Gajre <gajredeep@gmail.com>
2025-02-18 12:59:16 +0530

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Deep-Gajre", "_blank")
+            window.open("https://github.com/Deep-Gajre/project_ai_summarizer", "_blank")
           }
           className='black_btn'
         >
